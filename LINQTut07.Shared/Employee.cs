230a318d4c11917fad3a868fd458e1a7b37eee02
Metigator <metigator@gmail.com>
2022-01-27 21:44:03 -0500

    initial commit

---
+++ LINQTut07.Shared/Employee.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQTut07.Shared
+{
+    public class Employee
+    { 
+        public string  EmployeeNo { get; set; }
+
+        public string Name { get; set; } 
+         
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+
+        public List<string> Skills { get; set; } = new List<string>();
+
+         
+        public override string ToString()
+        {
+
+            return
+                    $"" +
+                    $"{EmployeeNo.PadRight(13, ' ')}\t" +
+                    $"{Name.PadRight(20, ' ')}\t" +
+                    $"{Email.PadRight(32, ' ')}\t"  +
+                    $"{String.Format("{0:C0}", Salary)}  "  +
+                    $"[ {string.Join(", ", Skills)} ]";
+
+        }
+
+
+        public override bool Equals(Object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Employee e = (Employee)obj;
+                return (this.Email.Equals(e.Email));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode() + 13 * 7;
+        }
+    }
+}
---
+++ LINQTut07/Program.cs
@@ -0,0 +1,110 @@
+﻿using LINQTut07.Shared;
+using System;
+using System.Linq;
+
+namespace LINQTut07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAny();
+            RunAll();
+            RunAnyAllQuerySyntax();
+            RunContain();
+            Console.ReadKey();
+        }
+        private static void RunAny()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+++++++++");
+            Console.WriteLine("Run Any()");
+            Console.WriteLine("+++++++++");
+            Console.WriteLine();
+
+
+            var employees = Repository.LoadEmployees();
+
+
+            // if any employee name starts with some sequence of letter
+            var input1 = "jac";
+            var result1 = employees.Any(e => e.Name.StartsWith(input1, StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine($"find employee with name starts with '{input1}' result: {result1}");
+
+            // if any employee employee salary less tham 1000
+            var input2 = 10000;
+            var result2 = employees.Any(e => e.Salary < input2);
+            Console.WriteLine($"at least one employee with salary less than {input2.ToString("C2")} result: {result2}");
+
+            // if any employee with skills less tham 1000
+            var noOfSkills = 1;
+            var result3 = employees.Any(e => e.Skills.Count < noOfSkills);
+            Console.WriteLine($"at least one employee with skill count less than {noOfSkills} result: {result3}");
+             
+        }
+
+        private static void RunAll()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+++++++++");
+            Console.WriteLine("Run All()");
+            Console.WriteLine("+++++++++"); 
+            Console.WriteLine();
+
+            var employees = Repository.LoadEmployees();
+
+            // if all employees have email defined
+            var result1 = employees.All(e => !string.IsNullOrWhiteSpace(e.Email));
+            Console.WriteLine($"All employees have email result: {result1}");
+
+            // if all employees have at least 1 skill
+            var result2 = employees.All(e => e.Skills.Any(x => x.Contains("C#")));
+            Console.WriteLine($"All employees have c# in their skills list result: {result2}");
+        }
+
+        private static void RunAnyAllQuerySyntax()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Run All() + Any() Query Expression");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+            var employees = Repository.LoadEmployees();
+
+            var result1 = from emp in employees
+                          where emp.Skills.All(x => x.Contains("c", StringComparison.OrdinalIgnoreCase))
+                          select emp;
+
+            result1.Print("Employees having skills  > skills");
+
+            var result2 = from emp in employees
+                          where emp.Skills.Any(x => x.Contains("node", StringComparison.OrdinalIgnoreCase))
+                          select emp;
+
+            result2.Print("Employees having skill node");
+        }
+
+        private static void RunContain()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++");
+            Console.WriteLine("Run Contain()");
+            Console.WriteLine("++++++++++++++");
+            Console.WriteLine();
+            var employees = Repository.LoadEmployees();
+
+
+            // if any employee contains 'ee' in his/her name
+
+            var e1 = employees.ToArray()[0];
+            var result1 = employees.Contains(e1);
+            Console.WriteLine($"find if any employee contains " +
+                $"'{e1.Email}' in his/her name result: {result1}");
+
+            var e2 = new Employee { Email = "Cole.Cochran02@example.com" };
+            var result2 = employees.Contains(e2);
+            Console.WriteLine($"find if any employee contains " +
+                $"'{e2.Email}' in his/her name result: {result2}");
+        }
+    }
+}
